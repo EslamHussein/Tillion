fc6fa36360072c43041415f93114336dffdead79
eslamhussein <eslam.hussein.awad@gmail.com>
2019-01-21 13:24:02 +0200

    Create add Target

---
+++ app/src/main/java/com/tremoloo/AddTargetActivity.kt
@@ -0,0 +1,22 @@
+package com.tremoloo
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_add_target.*
+
+
+class AddTargetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_target)
+        setSupportActionBar(addTargetToolbar)
+
+        categoriesRadioGroup.setOnPositionChangedListener { button, currentPosition, lastPosition ->
+            Toast.makeText(this@AddTargetActivity, "Position Changed! Position: $currentPosition", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+    }
+}
--- app/src/main/java/com/tremoloo/MainActivity.kt
+++ app/src/main/java/com/tremoloo/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.tremoloo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addTargetButton.setOnClickListener {
+            startActivity(Intent(this@MainActivity, AddTargetActivity::class.java))
+        }
     }
 }
