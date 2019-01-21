eed90bcac709f50e2a4588d4ba62dbe59f97df16
eslamhussein <eslam.hussein.awad@gmail.com>
2019-01-21 10:26:19 +0200

    init app create login screen - Splash screen

---
+++ app/src/main/java/com/tremoloo/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.tremoloo.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tremoloo.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/tremoloo/login/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.tremoloo.login
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import com.tremoloo.R
+
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+    }
+
+}
